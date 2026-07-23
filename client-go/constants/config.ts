04348c6396d5dd3ebb86459c289794325f8ffefa
jalazer <almanzar.j@northeastern.edu>
2026-07-23 11:08:46 -0400

    reset base url

--- client-go/constants/config.ts
+++ client-go/constants/config.ts
@@ -1,7 +1,5 @@
 export const USE_MOCK_DATA = true;
 
-export const API_WEB_URL = 'http://localhost:8000';
-
-export const API_BASE_URL = 'http://10.60.54.41:8000';
+export const API_BASE_URL = 'http://localhost:8000';
 
 export const STORAGE_KEY_USER_ID = 'firewatch_user_id';
